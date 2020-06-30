0446ff2fd6131589ba41206fa491e294a271b823
yukidaruma <yukidaruma@users.noreply.github.com>
2020-07-01 02:24:17 +0900

    Improve config typing

--- lib/config.dart
+++ lib/config.dart
@@ -2,15 +2,17 @@
 
 // ignore: avoid_classes_with_only_static_members
 class Config {
-  static bool _boolField(String key) => env[key] == 'true';
+  static String _stringField(String key) => env[key] as String;
+  static num _numericField(String key) => env[key] as num;
+  static bool _boolField(String key) => env[key] as bool ?? false;
 
   static Map<String, dynamic> env;
 
-  static String get DEV_IKSM_SESSION => env['DEV_IKSM_SESSION'];
-  static String get DEV_SALMON_STATS_API_TOKEN => env['DEV_SALMON_STATS_API_TOKEN'];
+  static String get DEV_IKSM_SESSION => _stringField('DEV_IKSM_SESSION');
+  static String get DEV_SALMON_STATS_API_TOKEN => _stringField('DEV_SALMON_STATS_API_TOKEN');
 
-  static String get SALMON_STATS_API_ORIGIN => env['SALMON_STATS_API_ORIGIN'];
-  static String get SALMON_STATS_URL => env['SALMON_STATS_URL'];
+  static String get SALMON_STATS_API_ORIGIN => _stringField('SALMON_STATS_API_ORIGIN');
+  static String get SALMON_STATS_URL => _stringField('SALMON_STATS_URL');
   static const String SALMON_IMAGE_BASE_PATH = 'https://splatoon-stats-api.yuki.games/static/images';
 
   static bool get HIDE_DEBUG_LABEL => _boolField('HIDE_DEBUG_LABEL');
