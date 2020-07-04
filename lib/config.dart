177870989e617a020b95e2c07dcc77c90b06899e
yukidaruma <yukidaruma@users.noreply.github.com>
2020-07-05 03:39:18 +0900

    refactor(internal): Reorder `Config` fields and add comments on `DEV_` fields

--- lib/config.dart
+++ lib/config.dart
@@ -10,10 +10,6 @@ class Config {
 
   static Map<String, dynamic> env;
 
-  static String get DEV_IKSM_SESSION => _stringField('DEV_IKSM_SESSION');
-  static String get DEV_SALMON_STATS_API_TOKEN => _stringField('DEV_SALMON_STATS_API_TOKEN');
-  static bool get DEV_OBSCURE_IKSM_SESSION => _boolField('DEV_OBSCURE_IKSM_SESSION');
-
   static String get SALMON_STATS_API_ORIGIN => _stringField('SALMON_STATS_API_ORIGIN');
   static String get SALMON_STATS_URL => _stringField('SALMON_STATS_URL');
   static const String SALMON_IMAGE_BASE_PATH = 'https://splatoon-stats-api.yuki.games/static/images';
@@ -24,6 +20,12 @@ class Config {
   static const String SPLATNET_ORIGIN = 'https://app.splatoon2.nintendo.net';
   static const String SPLATNET_API_ORIGIN = '$SPLATNET_ORIGIN/api';
   static const String SPLATNET_USER_AGENT = 'OnlineLounge/1.6.1.2 NASDKAPI Android';
+
+  // These values are intended for development purpose.
+  // DO NOT USE for production code.
+  static String get DEV_IKSM_SESSION => _stringField('DEV_IKSM_SESSION');
+  static bool get DEV_OBSCURE_IKSM_SESSION => _boolField('DEV_OBSCURE_IKSM_SESSION');
+  static String get DEV_SALMON_STATS_API_TOKEN => _stringField('DEV_SALMON_STATS_API_TOKEN');
 
   static Locale get DEV_LOCALE {
     final String languageCode = _stringField('DEV_LOCALE');
