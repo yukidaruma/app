2c91dc7e6f55cd30895a945256fe25653d59a41c
yukidaruma <yukidaruma@users.noreply.github.com>
2020-08-06 00:17:04 +0900

    ci: Update GitHub Actions to run on release branch push instead of tag

--- scripts/commit-release.sh
+++ scripts/commit-release.sh
@@ -1,11 +1,8 @@
-# Discard overridden .env values
-cp .env assets/.env
-
 # Only changes to CHANGELOG.md is allowed
-CHANGED_FILES=$(git status --porcelain -- ':!CHANGELOG.md' | wc -l)
+CHANGED_FILES=$(git status --porcelain -- ':!CHANGELOG.md' ':!pubspec.yaml' | wc -l)
 if [ "$CHANGED_FILES" -gt 0 ]; then
   echo "Error: Please commit or stash uncommited changes before running release script."
-  echo "Note: Changes to CHANGELOG.md are allowed."
+  echo "Note: Changes to \`CHANGELOG.md\` / \`pubspec.yaml\` are allowed."
   exit
 fi
 
@@ -15,6 +12,6 @@ RELEASE_NOTES=$(git diff -U0 -- CHANGELOG.md | grep '^\+' | grep -Ev '^\+{2,} b/
 NEW_VERSION=$(head -1 CHANGELOG.md | sed -e 's/^# Version \(.\+\) .\+$/\1/')
 
 < CHANGELOG.md dart "$SCRIPT_DIR/convert-markdown.dart" > assets/release_notes.html
-git add CHANGELOG.md assets/release_notes.html
+git add CHANGELOG.md assets/release_notes.html pubspec.yaml
 git commit -m "Release $NEW_VERSION"
-git -c core.commentChar=";" tag -a "v$NEW_VERSION" -m "'$RELEASE_NOTES'"
+git -c core.commentChar=";" tag -a "v$NEW_VERSION" -m "$RELEASE_NOTES"
