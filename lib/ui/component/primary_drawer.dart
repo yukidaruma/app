yukidaruma/app

commit 213f1aab2a99d1feac9b4c7c8de2f71b2d90afc0

fix: Make it possible to update iksm_session after expiration
Close #26

lib/ui/page/enter_iksm_session_page.dart
@@ -9,13 +9,20 @@ import 'package:salmon_stats_app/store/global.dart';
 import 'package:salmon_stats_app/ui/all.dart';
 import 'package:salmon_stats_app/util/all.dart';
 
-class EnterIksmPage extends StatefulWidget {
+class EnterIksmPage extends StatefulWidget implements PushablePage {
   const EnterIksmPage({this.restartOnComplete = false});
 
   final bool restartOnComplete;
 
   @override
   _EnterIksmPageState createState() => _EnterIksmPageState();
+
+  Future<void> push(BuildContext context) {
+    return Navigator.push(
+      context,
+      MaterialPageRoute<void>(builder: (_) => this),
+    );
+  }
 }
 
 class _EnterIksmPageState extends State<EnterIksmPage> {
@@ -98,6 +105,7 @@ class _EnterIksmPageState extends State<EnterIksmPage> {
         ..cookieJar = cookieJar;
 
       if (widget.restartOnComplete) {
+        store.iksmValidityFuture = validateIksmSession(cookieJar);
         await store.switchProfile(profile);
         Navigator.pop(context);
       }

lib/ui/page/results_page.dart
@@ -78,6 +78,31 @@ class _ResultsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilderWrapper<bool>(
+      future: context.select((GlobalStore store) => store.iksmValidityFuture),
+      builder: (BuildContext context, bool isIksmValid) {
+        if (!isIksmValid) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ErrorText(S.of(context).iksmExpirationMessage),
+                const Padding(padding: EdgeInsets.only(bottom: 8.0)),
+                RaisedButton(
+                  child: Text(S.of(context).iksmExpirationUpdateButtonLabel),
+                  onPressed: () => const EnterIksmPage(restartOnComplete: true).push(context),
+                ),
+              ],
+            ),
+          );
+        }
+
+        return _buildContent(context);
+      },
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
     Widget makeRefreshable(Widget child) {
       return RefreshIndicator(
         key: _refreshIndicatorKey,

lib/generated/intl/messages_en.dart
@@ -40,6 +40,8 @@ class MessageLookup extends MessageLookupByLibrary {
     "fail" : MessageLookupByLibrary.simpleMessage("Fail"),
     "getApiToken" : MessageLookupByLibrary.simpleMessage("Get API Token"),
     "goBack" : MessageLookupByLibrary.simpleMessage("Go back"),
+    "iksmExpirationMessage" : MessageLookupByLibrary.simpleMessage("iksm_session has expired."),
+    "iksmExpirationUpdateButtonLabel" : MessageLookupByLibrary.simpleMessage("Update iksm_session."),
     "iksmSession" : MessageLookupByLibrary.simpleMessage("iksm_session"),
     "iksmSessionAlreadyUsed" : MessageLookupByLibrary.simpleMessage("You already have added this iksm_session."),
     "iksmSessionPromptText" : MessageLookupByLibrary.simpleMessage("Enter your iksm_session"),

lib/generated/intl/messages_ja.dart
@@ -36,6 +36,8 @@ class MessageLookup extends MessageLookupByLibrary {
     "errorDialogTitle" : MessageLookupByLibrary.simpleMessage("エラー"),
     "getApiToken" : MessageLookupByLibrary.simpleMessage("API Tokenを取得"),
     "goBack" : MessageLookupByLibrary.simpleMessage("戻る"),
+    "iksmExpirationMessage" : MessageLookupByLibrary.simpleMessage("iksm_session の有効期限が切れています。"),
+    "iksmExpirationUpdateButtonLabel" : MessageLookupByLibrary.simpleMessage("iksm_session を更新する"),
     "iksmSessionAlreadyUsed" : MessageLookupByLibrary.simpleMessage("この iksm_session はすでに追加されています。"),
     "iksmSessionPromptText" : MessageLookupByLibrary.simpleMessage("iksm_session を入力してください"),
     "invalidIksmSession" : MessageLookupByLibrary.simpleMessage("iksm_session が正しくありません。入力内容を確認の上、もう一度やり直してください。"),

lib/generated/l10n.dart
@@ -133,6 +133,26 @@ class S {
     );
   }
 
+  /// `iksm_session has expired.`
+  String get iksmExpirationMessage {
+    return Intl.message(
+      'iksm_session has expired.',
+      name: 'iksmExpirationMessage',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Update iksm_session.`
+  String get iksmExpirationUpdateButtonLabel {
+    return Intl.message(
+      'Update iksm_session.',
+      name: 'iksmExpirationUpdateButtonLabel',
+      desc: '',
+      args: [],
+    );
+  }
+
   /// `Enter your iksm_session`
   String get iksmSessionPromptText {
     return Intl.message(

lib/l10n/intl_en.arb
@@ -10,6 +10,8 @@
   "confirmCancelUploadingYes": "Yes, stop uploading",
   "confirmRemoveAccount": "Are you sure you want to remove account \"{name}\"?",
   "continueButtonText": "Continue",
+  "iksmExpirationMessage": "iksm_session has expired.",
+  "iksmExpirationUpdateButtonLabel": "Update iksm_session.",
   "iksmSessionPromptText": "Enter your iksm_session",
   "openOtherPage": "Open {pageName}",
   "noResultsFound": "No results were found. Please play some Salmon Run and refresh the page.",

lib/l10n/intl_ja.arb
@@ -10,6 +10,8 @@
   "confirmCancelUploadingYes": "中断する",
   "confirmRemoveAccount": "本当に\"{name}\"のアカウントを削除しますか?",
   "continueButtonText": "続ける",
+  "iksmExpirationMessage": "iksm_session の有効期限が切れています。",
+  "iksmExpirationUpdateButtonLabel": "iksm_session を更新する",
   "iksmSessionPromptText": "iksm_session を入力してください",
   "noResultsFound": "リザルトが見つかりませんでした。サーモンランをプレイして、ページを更新してください。",
   "openOtherPage": "{pageName}を開く",

lib/main.dart
@@ -43,6 +43,7 @@ Future<void> main() async {
 
   final GlobalStore globalStore = GlobalStore(
     cookieJar: cookieJar,
+    iksmValidityFuture: validateIksmSession(cookieJar),
     packageInfoFuture: PackageInfo.fromPlatform(),
     profiles: profiles,
   );

commit 02d8ff1197229435af2ec52646dd3f8310d2ba7f

fix: Fix entering new `iksm_session` may link `iksm_session` to incorrect `pid`
This issue was introduced in 213f1aab2a99d1feac9b4c7c8de2f71b2d90afc0.

lib/ui/component/primary_drawer.dart
@@ -150,7 +150,7 @@ class _PrimaryDrawerState extends State<PrimaryDrawer> {
     return Navigator.push(
       context,
       MaterialPageRoute<void>(
-        builder: (_) => const EnterIksmPage(restartOnComplete: true),
+        builder: (_) => const EnterIksmPage(type: EnterIksmPageType.addAccount),
       ),
     );
   }

lib/ui/home_page.dart
@@ -30,7 +30,7 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     if (Provider.of<GlobalStore>(context).cookieJar == null) {
-      return const EnterIksmPage();
+      return const EnterIksmPage(type: EnterIksmPageType.firstLogin);
     }
 
     final Widget scaffold = Scaffold(

lib/ui/page/enter_iksm_session_page.dart
@@ -9,10 +9,16 @@ import 'package:salmon_stats_app/store/global.dart';
 import 'package:salmon_stats_app/ui/all.dart';
 import 'package:salmon_stats_app/util/all.dart';
 
-class EnterIksmPage extends StatefulWidget implements PushablePage {
-  const EnterIksmPage({this.restartOnComplete = false});
+enum EnterIksmPageType {
+  addAccount,
+  firstLogin,
+  sessionTimeOut,
+}
 
-  final bool restartOnComplete;
+class EnterIksmPage extends StatefulWidget implements PushablePage {
+  const EnterIksmPage({@required this.type});
+
+  final EnterIksmPageType type;
 
   @override
   _EnterIksmPageState createState() => _EnterIksmPageState();
@@ -97,14 +103,18 @@ class _EnterIksmPageState extends State<EnterIksmPage> {
         ..iksmSession = iksmSession
         ..avatar = avatar;
 
-      await UserProfileRepository(DatabaseProvider.instance).create(profile);
-
       final GlobalStore store = context.read<GlobalStore>();
       store
         ..addProfile(profile)
         ..cookieJar = cookieJar;
 
-      if (widget.restartOnComplete) {
+      if (widget.type == EnterIksmPageType.sessionTimeOut) {
+        await UserProfileRepository(DatabaseProvider.instance).save(profile);
+      } else {
+        await UserProfileRepository(DatabaseProvider.instance).create(profile);
+      }
+
+      if (widget.type != EnterIksmPageType.firstLogin) {
         store.iksmValidityFuture = validateIksmSession(cookieJar);
         await store.switchProfile(profile);
         Navigator.pop(context);

lib/ui/page/results_page.dart
@@ -94,7 +94,7 @@ class _ResultsList extends StatelessWidget {
                 const Padding(padding: EdgeInsets.only(bottom: 8.0)),
                 RaisedButton(
                   child: Text(S.of(context).iksmExpirationUpdateButtonLabel),
-                  onPressed: () => const EnterIksmPage(restartOnComplete: true).push(context),
+                  onPressed: () => const EnterIksmPage(type: EnterIksmPageType.sessionTimeOut).push(context),
                 ),
               ],
             ),